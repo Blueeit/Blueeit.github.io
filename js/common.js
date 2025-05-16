4548961bf6833fb5a8d891a5397d080f8882ac3e
albert.kim-at-942453141665 <albert.kim@bucketerm.com>
2025-05-16 17:07:55 +0900

    .

--- js/common.js
+++ js/common.js
@@ -1,1 +1,12 @@
-test
+$(document).ready(function () {
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 50) {
+      $('#navbar').addClass('scrolled');
+      // $('.logo-wrap img').attr('src', 'images/logo-txt-wh.svg');
+
+    } else {
+      $('#navbar').removeClass('scrolled');
+      //$('.logo-wrap img').attr('src', 'images/logo-txt-bk.svg');
+    }
+  });
+});
