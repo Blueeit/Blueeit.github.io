0b134f7eb8950f62a87a701f04b12a787ed10932
Blueet <pji762@naver.com>
2026-05-08 15:54:56 +0900

    260508 메인페이지 nav,img 변경

--- js/common.js
+++ js/common.js
@@ -9,11 +9,40 @@ $(document).ready(function () {
     }
   });
 
-  //nav 모바일
+  //nav 
+  $('.main-nav').hover(
+    function(){
+    if($(window).width() > 565){
+        $(this).children('.sub-nav').stop().fadeIn(200);
+    }
+},
+    function() {
+         if($(window).width() > 565){
+        $(this).children('.sub-nav').stop().fadeOut(200);
+        }
+    }
+  );
+
+  $('.main-nav:has(.sub-nav)').click(function () {
+    if($(window).width() <= 565) {
+        $(this).children('.sub-nav').stop().slideToggle(200);
+    }
+  });
+
   $('.mobile-menu-btn').on('click', function () {
     $(this).toggleClass('active');
     $('.nav-wrap nav').toggleClass('is-open');
   });
+
+  $('.nav-menu a').on('click', function() {
+    if($(window).width() <= 565){
+        $('.nav-overlay').removeClass('is-open');
+
+        $('.mobile-menu-btn').removeClass('active');
+
+        $('.sub-nav').hide();
+    }
+  })
 
   //nav 베경 클릭 > 닫기
   $('.nav-overlay').on('click', function() {
@@ -25,4 +54,6 @@ $(document).ready(function () {
   $('.nav-overlay .nav-menu').on('click', function(e) {
     e.stopPropagation();
   });
+
+  
 });
